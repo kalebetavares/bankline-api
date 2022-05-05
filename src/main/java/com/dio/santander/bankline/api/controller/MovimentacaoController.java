92cef9f6242a3ba35c6ba3a92559a0ef00199653
kalebetavares <kalebe.tavares@gmail.com>
2022-05-05 19:00:36 -0300

    Projeto final

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +26,13 @@ public class MovimentacaoController {
 	private MovimentacaoService service;
 	
 	@GetMapping
-	public List <Movimentacao> findAll(){
+	public List<Movimentacao> findAll(){
 		return repository.findAll();
+	}
+	
+	@GetMapping("/{idConta}")
+	public List<Movimentacao> findAll(@PathVariable("idConta") Integer idConta){
+			return repository.findByIdConta(idConta);
 	}
 	
 	@PostMapping
